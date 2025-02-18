3678af14db018e6fccda6c81d4714bbc3d5e6ff6
Silvhr <silvhr@protonmail.com>
2025-02-17 19:07:33 -0600

    added firebase login template flow

--- lib/app.dart
+++ lib/app.dart
@@ -1,26 +1,43 @@
+// import 'package:flutter/material.dart';
+// import 'src/views/role_selection_screen.dart';
+// import 'src/views/realtor_login_screen.dart';
+// import 'src/views/investor_login_screen.dart';
+// import 'src/views/realtor_home_screen.dart';
+// import 'src/views/investor_home_screen.dart';
+
 import 'package:flutter/material.dart';
-import 'src/views/role_selection_screen.dart';
-import 'src/views/realtor_login_screen.dart';
-import 'src/views/investor_login_screen.dart';
-import 'src/views/realtor_home_screen.dart';
-import 'src/views/investor_home_screen.dart';
+import 'auth_gate.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Property Investment App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      // The first screen the user sees is the role selection screen.
-      home: RoleSelectionScreen(),
-      routes: {
-        '/realtorLogin': (context) => RealtorLoginScreen(),
-        '/investorLogin': (context) => InvestorLoginScreen(),
-        '/realtor': (context) => RealtorHomeScreen(),
-        '/investor': (context) => InvestorHomeScreen(),
-      },
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AuthGate(),
     );
   }
 }
+
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Property Investment App',
+//       theme: ThemeData(primarySwatch: Colors.blue),
+//       // The first screen the user sees is the role selection screen.
+//       home: RoleSelectionScreen(),
+//       routes: {
+//         '/realtorLogin': (context) => RealtorLoginScreen(),
+//         '/investorLogin': (context) => InvestorLoginScreen(),
+//         '/realtor': (context) => RealtorHomeScreen(),
+//         '/investor': (context) => InvestorHomeScreen(),
+//       },
+//     );
+//   }
+// }
