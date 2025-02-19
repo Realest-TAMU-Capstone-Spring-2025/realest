3022327fae1ca2cbb249f156fd06d88c96b5e14e
EshwarReddy13 <eshwarreddygadi@gmail.com>
2025-02-18 18:33:56 -0600

    Refractored folder and file names, Integrated the Firebase Auth template with our UI, Added an invitation code screen for first time users (Investors)

--- lib/src/views/role_selection_screen.dart
+++ lib/src/views/role_selection_screen.dart
@@ -1,106 +1,101 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
-
+import '../config/global_variables.dart';
+import 'home.dart';
 
 class RoleSelectionScreen extends StatelessWidget {
   const RoleSelectionScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Retrieve screen dimensions
-    final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
-
     return Scaffold(
-      backgroundColor: Color(0xFFf2f6fc),
-      // appBar: AppBar(
-      //   title: Text('Select Your Role'),
-      // ),
-      body: Stack(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Logo aligned to top left
+              Align(
+                alignment: Alignment.topLeft,
+                child: Image.asset(
+                  'assets/images/logo.png',
+                  width: 50,
+                  height: 50,
+                ),
+              ),
+              // Lottie animation (building.json) moved a bit more up
+              Transform.translate(
+                offset: const Offset(0, -40),
+                child:Lottie.asset(
+                  'assets/lottie/building.json',
+                  width: 350,
+                  height: 400,
+                ),
+              ),
 
-        children: [
-          Positioned(
-            left: screenWidth * 0.03, // 20% from the left
-            top: screenHeight * 0.05, // 10% from the top
-            child: Image.asset(
-              'assets/images/logo.png',
-              width: 50,
-              height: 50,
-            ),
-          ),
-          // Custom position for the text
-          Positioned(
-              left: screenWidth * 0.08, // 10% from the right
-              bottom: screenHeight * 0.50, // 50% from the top,
-              child: Lottie.asset('assets/lottie/building.json',
-                width: 350,
-                height: 400,
-              )
-          ),
-          Positioned(
-            left: screenWidth * 0.065, // 10% from the left
-            top: screenHeight * 0.4, // 35% from the top
-            child: Text(
-                'Welcome to RealEst',
-                style: GoogleFonts.roboto(fontSize: 40, fontWeight: FontWeight.bold)
-            ),
-          ),
-          Positioned(
-            left: screenWidth * 0.15, // 10% from the left
-            top: screenHeight * 0.45, // 35% from the top
-            child: Text(
-              'Are you a Realtor or Investor?',
-              style: GoogleFonts.roboto(fontSize: 22,color: Colors.grey)
-            ),
-          ),
-          // Custom position for the Realtor button
-          Positioned(
-            left: screenWidth * 0.1,// 10% from the left
-            top: screenHeight * 0.65, // 50% from the top
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/realtorLogin');
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blueGrey, // Background color
-                foregroundColor: Colors.white, // Text color
-                padding: EdgeInsets.symmetric(horizontal: 120, vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                textStyle: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
+              Transform.translate(
+                offset: const Offset(0, -130),
+                child:Text(
+                  'Welcome to RealEst',
+                  style: GoogleFonts.poppins(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
               ),
-              child: Text('Realtor'),
-            )
-          ),
-          // Custom position for the Investor button
-          Positioned(
-            left: screenWidth * 0.1, // 10% from the right
-            top: screenHeight * 0.75, // 50% from the top
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/investorLogin');
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blueGrey, // Background color
-                foregroundColor: Colors.white, // Text color
-                padding: EdgeInsets.symmetric(horizontal: 120, vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                textStyle: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
+              // Additional text
+              Transform.translate(
+                offset: const Offset(0, -130),
+                child:Text(
+                  'Are you a Realtor or Investor?',
+                  style: GoogleFonts.poppins(fontSize: 22, color: Colors.grey),
+                  textAlign: TextAlign.center,
                 ),
               ),
-              child: Text('Investor'),
-            ),
+
+              const SizedBox(height: 30),
+              // Realtor button
+              ElevatedButton(
+                onPressed: () {
+                  userRole = 'Realtor';
+                  Navigator.pushReplacementNamed(context, '/customLogin');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF212834),
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 120, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  textStyle: GoogleFonts.openSans(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+                child: const Text('Realtor'),
+              ),
+              const SizedBox(height: 50),
+              // Investor button
+              ElevatedButton(
+                onPressed: () {
+                  userRole = 'Investor';
+                  Navigator.pushReplacementNamed(context, '/customLogin');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF212834),
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 115, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  textStyle: GoogleFonts.openSans(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+                child: const Text('Investor'),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
